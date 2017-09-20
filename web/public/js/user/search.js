29aa366398cfc425d0843be5735834b25f47b281
Hug Hoo <HugHoo@outlook.com>
2017-09-20 22:48:08 +0800

    search.html article question

---
+++ web/public/js/user/search.js
@@ -0,0 +1,66 @@
+let app = angular.module("app", ['ngSanitize']).config(function($locationProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+});
+
+app.controller("appCtrl", function ($scope, $http){
+    
+    console.log("app controller loaded.");
+
+    $scope.search = function(){
+
+        let val = $("#search_content").val();
+        console.log(val);
+
+        // 搜索问题
+        $http({
+            url : "../getQuestionsByKeyword.action" ,
+            method : "get" ,
+            params : {
+                keyword : val,
+                maxNumInOnePage : 5,
+                pageNum : 1
+            } ,
+        }).then(function(resp){
+            console.log(resp);
+
+            $scope.questionList = resp.data;
+
+            for(var i in $scope.questionList){
+                $scope.questionList[i].content = markdown.toHTML($scope.questionList[i].content);
+            }
+        }, function(resp){
+            httpErr(resp);
+        });
+
+        // 搜索文章
+        $http({
+            url : "../getArticlesByKeyword.action" ,
+            method : "get" ,
+            params : {
+                keyword : val,
+                maxNumInOnePage : 5,
+                pageNum : 1
+            } ,
+        }).then(function(resp){
+            console.log(resp);
+
+            $scope.articleList = resp.data;
+
+            for(var i in $scope.articleList){
+                $scope.articleList[i].content = markdown.toHTML($scope.articleList[i].content);
+            }
+        }, function(resp){
+            httpErr(resp);
+        });
+
+    }
+
+});
+
+function httpErr(msg) {
+    console.log("http status : " + msg.status);
+    console.log("http statusText : " + msg.statusText);
+}
