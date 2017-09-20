da4a6a5dd286a1b020b22ea979be697aa80ae476
Hug Hoo <HugHoo@outlook.com>
2017-09-20 16:39:11 +0800

    question.html .js

---
+++ web/public/js/user/article.js
@@ -0,0 +1,41 @@
+let app = angular.module("app", []).config(function($locationProvider) {
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+});
+
+app.controller("appCtrl", function ($scope, $http, $location){
+
+    console.log("app controller loaded.");
+
+    // TODO get session
+
+    // 获取文章id
+    console.log($location.search());
+
+    let article_id = $location.search().id;
+
+    $http({
+        url : "../getArticle.action" ,
+        method : "get" ,
+        params : {
+            id : article_id
+        } ,
+    }).then(function(resp){
+        console.log(resp);
+
+        $scope.article = resp.data;
+
+        $("#article_content").get(0).innerHTML = markdown.toHTML(resp.data.content);
+
+    }, function(resp){
+        httpErr(resp);
+    });
+
+});
+
+function httpErr(msg) {
+    console.log("http status : " + msg.status);
+    console.log("http statusText : " + msg.statusText);
+}
