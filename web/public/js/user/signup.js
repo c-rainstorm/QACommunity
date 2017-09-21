db973ded1e9eefde7107614f11c1e89bf1a6e6c3
Hug Hoo <HugHoo@outlook.com>
2017-09-22 03:16:59 +0800

    avatar resolution for now

--- web/public/js/user/signup.js
+++ web/public/js/user/signup.js
@@ -2,6 +2,58 @@ let app = angular.module("app", []);
 
 app.controller("appCtrl", function ($scope, $http, $window, $timeout) {
 
+    let avatarList = [
+        "0.png",
+        "1.png",
+        "2.png",
+        "3.png",
+        "4.png",
+        "5.png",
+        "6.png",
+        "7.png",
+        "8.png",
+        "9.png",
+        "10.png",
+        "11.png",
+        "12.png",
+        "13.png",
+        "14.png",
+        "15.png",
+        "16.png",
+        "17.png",
+        "18.png",
+        "19.png",
+        "20.png",
+        "21.png",
+        "22.png",
+        "23.png",
+        "24.png",
+        "25.png",
+        "26.png",
+        "27.png",
+        "28.png",
+        "29.png",
+        "30.png",
+        "31.png",
+        "32.png",
+        "33.png",
+        "34.png",
+        "35.png",
+        "36.png",
+        "37.png",
+        "38.png",
+        "39.png",
+        "40.png",
+        "41.png",
+        "42.png",
+        "43.png",
+        "44.png",
+        "45.png",
+        "46.png",
+        "47.png",
+        "48.png"
+    ];
+
     console.log("app ctronller loaded.");
 
     $scope.user = {
@@ -31,6 +83,9 @@ app.controller("appCtrl", function ($scope, $http, $window, $timeout) {
             return;
         }
 
+        let iTmp = parseInt(Math.random() * (38 - 26) + 26);
+        $scope.user.avatar = avatarList[iTmp];
+
         $http({
             url : "../addUser.action",
             method : "post",
@@ -40,7 +95,7 @@ app.controller("appCtrl", function ($scope, $http, $window, $timeout) {
             let data = resp.data;
             if(data.result == "true"){
                 console.log("singup successed.");
-                toastr.info("Singup successed.");
+                toastr.info("注册成功.");
 
                 $timeout(function(){
                     $window.location.href = "./home.html";
