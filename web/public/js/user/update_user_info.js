7fff2f2ab3e3038a0f6679fc1af87517c38a6275
Hug Hoo <HugHoo@outlook.com>
2017-09-21 22:43:46 +0800

    update_user_info, not check

--- web/public/js/user/update_user_info.js
+++ web/public/js/user/update_user_info.js
@@ -1,8 +1,13 @@
 let app = angular.module("app", []);
 
-app.controller("appCtrl", function($scope, $http){
+app.controller("appCtrl", function($scope, $http, $window, $timeout){
 
     console.log("app controller loaded.");
+
+    $scope.sex = {
+        male : false,
+        female : true
+    };
 
     $scope.session = {};
     // 获取 session
@@ -38,6 +43,53 @@ app.controller("appCtrl", function($scope, $http){
         }, function(resp){
             httpErr(resp);
         });
+
+        // 更新用户信息
+        $scope.updateUserInfoSubmit = function(){
+
+            console.log("update...");
+
+            $http({
+                url: "../updateUser.action",
+                method: "post",
+                data: angular.copy($scope.user)
+            }).then(function(resp){
+                console.log(resp);
+
+                if(resp.data.result == "true"){
+                    toastr.success("个人信息更新成功");
+
+                    $timeout(function(){
+                        $window.location.href = "./home.html";
+                    }, 1000);
+                }else{
+                    toastr.error("个人信息更新失败");
+                }
+            }, function(resp){
+                httpErr(resp);
+            });
+
+            // let $elem = $("#avatar");
+            // let f = $elem[0].files[0];
+            // console.log($elem[0].files[0]);
+    
+            // if(f == undefined){
+            //     console.log("there is no file need to be uploaded.");
+
+
+            // }else{
+            //     let fr = new FileReader();
+            //     fr.onloadend = function(){
+            //         let avatar_file = fr.result;
+            //         console.log("reading file.");
+        
+                    
+            //     }
+        
+            //     fr.readAsBinaryString(f);
+            // }
+
+        }
     
     }, function(resp){
         httpErr(resp);
